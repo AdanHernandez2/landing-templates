e4da599a3cf9f8e2f94a7e5a6e811527e18858c4
script <hernandezluis0219@gmail.com>
2023-04-18 17:49:02 -0400

    web coffee

---
+++ web-coffee/gulpfile.js
@@ -0,0 +1,25 @@
+const { src, dest, watch, series, parallel } = require('gulp'); // significa que esporta mas de una funcion
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+function css(done) {
+    //compilar sass
+    // pasos: 1- Identificar archivo. 2- compilarla. 3-Guardar el .css
+
+    src('src/scss/app.scss')
+        .pipe( sass() ) //compila
+        .pipe(postcss([autoprefixer()]))
+        .pipe( dest('build/css') ) //almacena hoja de estilos compilada
+    done();
+}
+
+function dev( done ) {
+    watch('src/scss/app.scss', css);
+
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
+exports.default = series(css, dev);

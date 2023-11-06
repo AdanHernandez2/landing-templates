53bc748abd197859db2d280e2e72b9d2ccfa4721
script <hernandezluis0219@gmail.com>
2023-11-05 21:41:21 -0400

    header web coffee

--- web-coffee/gulpfile.js
+++ web-coffee/gulpfile.js
@@ -1,7 +1,13 @@
 const { src, dest, watch, series, parallel } = require('gulp'); // significa que esporta mas de una funcion
+
+// css sass
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+//imagenes
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     //compilar sass
@@ -14,12 +20,43 @@ function css(done) {
     done();
 }
 
+function imagenes(done) {
+    //importacion dinamica de imagemin
+    import('gulp-imagemin').then((imagemin) => {
+      src('src/img/**/*')
+        .pipe(imagemin.default({optimizationLevel: 3}))
+        .pipe(dest('build/img'))
+      done();
+    });
+}  
+
+function versionWebp() {
+    const opciones = {
+        quality:50
+    }
+    return src('src/img/**/*.{png,jpg}')
+            .pipe( webp(opciones))
+            .pipe( dest('build/img'))
+}
+
+function versionAvif() {
+    const opciones = {
+        quality:50
+    }
+    return src('src/img/**/*.{png,jpg}')
+            .pipe( avif(opciones))
+            .pipe( dest('build/img'))
+}
+
 function dev( done ) {
     watch('src/scss/**/*.scss', css);
-
+    watch('src/img/**/*', imagenes);
     done();
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css, dev);
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
